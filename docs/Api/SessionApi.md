eaafd7b9aa2842df2c03f93847a94870971e9135
Alexandru-Daniel Nedelcu <alexnedelcu0@gmail.com>
2018-10-17 15:43:31 +0200

    Added deleteSession method

--- docs/Api/SessionApi.md
+++ docs/Api/SessionApi.md
@@ -5,7 +5,7 @@ All URIs are relative to *https://checkout-api.reepay.com*
 Method | HTTP request | Description
 ------------- | ------------- | -------------
 [**createSession**](SessionApi.md#createSession) | **POST** /v1/session/charge | Create charge session
-[**deleteSession**](SessionApi.md#deleteSession) | **DELET** /v1/session/{id} | Delete session
+[**deleteSession**](SessionApi.md#deleteSession) | **DELETE** /v1/session/{id} | Delete session
 
 # **createSession**
 > \Reepay\Client\Model\Session createSession($body)
@@ -73,6 +73,59 @@ Name | Type | Description  | Notes
 ### HTTP request headers
 
  - **Content-Type**: application/json
+ - **Accept**: application/json
+
+[[Back to top]](#) [[Back to API list]](../../README.md#documentation-for-api-endpoints) [[Back to Model list]](../../README.md#documentation-for-models) [[Back to README]](../../README.md)
+
+# **deleteSession**
+> \Reepay\Model\Session deleteSession($id)
+
+Delete session
+
+
+
+### Example
+```php
+<?php
+require_once(__DIR__ . '/vendor/autoload.php');
+
+// Configure API key authorization: apiKey
+Reepay\Configuration::getDefaultConfiguration()->setApiKey('X-Auth-Token', 'YOUR_API_KEY');
+// Uncomment below to setup prefix (e.g. Bearer) for API key, if needed
+// Reepay\Configuration::getDefaultConfiguration()->setApiKeyPrefix('X-Auth-Token', 'Bearer');
+// Configure HTTP basic authorization: basicAuth
+Reepay\Configuration::getDefaultConfiguration()->setUsername('YOUR_USERNAME');
+Reepay\Configuration::getDefaultConfiguration()->setPassword('YOUR_PASSWORD');
+
+$apiInstance = new Reepay\Api\SessionApi();
+$id = "id_example"; // string | Session id
+
+try {
+    $result = $apiInstance->deleteSession($id);
+    print_r($result);
+} catch (Exception $e) {
+    echo 'Exception when calling SessionApi->deleteSession: ', $e->getMessage(), PHP_EOL;
+}
+?>
+```
+
+### Parameters
+
+Name | Type | Description  | Notes
+------------- | ------------- | ------------- | -------------
+ **id** | **string**| Session id |
+
+### Return type
+
+[**\Reepay\Model\Session**](../Model/Session.md)
+
+### Authorization
+
+[apiKey](../../README.md#apiKey), [basicAuth](../../README.md#basicAuth)
+
+### HTTP request headers
+
+ - **Content-Type**: Not defined
  - **Accept**: application/json
 
 [[Back to top]](#) [[Back to API list]](../../README.md#documentation-for-api-endpoints) [[Back to Model list]](../../README.md#documentation-for-models) [[Back to README]](../../README.md)
--- lib/Api/SessionApi.php
+++ lib/Api/SessionApi.php
@@ -176,4 +176,116 @@ class SessionApi
             throw $e;
         }
     }
+
+    /**
+     * Operation deleteSession
+     *
+     * Delete session
+     *
+     * @param string $id Session id (required)
+     * @throws \Reepay\ApiException on non-2xx response
+     * @return \Reepay\Model\Session
+     */
+    public function deleteSession($id)
+    {
+        list($response) = $this->deleteSessionWithHttpInfo($id);
+
+        return $response;
+    }
+
+    /**
+     * Operation deleteSessionWithHttpInfo
+     *
+     * Delete session
+     *
+     * @param string $id Session id (required)
+     * @throws \Reepay\ApiException on non-2xx response
+     * @return array of \Reepay\Model\Session, HTTP status code, HTTP response headers (array of strings)
+     */
+    public function deleteSessionWithHttpInfo($id)
+    {
+        // verify the required parameter 'id' is set
+        if ($id === null) {
+            throw new \InvalidArgumentException('Missing the required parameter $id when calling deleteSession');
+        }
+        // parse inputs
+        $resourcePath = "/v1/session/{id}";
+        $httpBody = '';
+        $queryParams = [];
+        $headerParams = [];
+        $formParams = [];
+        $_header_accept = $this->apiClient->selectHeaderAccept(['application/json']);
+        if (!is_null($_header_accept)) {
+            $headerParams['Accept'] = $_header_accept;
+        }
+        $headerParams['Content-Type'] = $this->apiClient->selectHeaderContentType([]);
+
+        // path params
+        if ($id !== null) {
+            $resourcePath = str_replace(
+                "{" . "id" . "}",
+                $this->apiClient->getSerializer()->toPathValue($id),
+                $resourcePath
+            );
+        }
+
+        // for model (json/xml)
+        if (isset($_tempBody)) {
+            $httpBody = $_tempBody; // $_tempBody is the method argument, if present
+        } elseif (count($formParams) > 0) {
+            $httpBody = $formParams; // for HTTP post (form)
+        }
+        // this endpoint requires API key authentication
+        $apiKey = $this->apiClient->getApiKeyWithPrefix('X-Auth-Token');
+        if (strlen($apiKey) !== 0) {
+            $headerParams['X-Auth-Token'] = $apiKey;
+        }
+        // this endpoint requires HTTP basic authentication
+        if (strlen($this->apiClient->getConfig()->getUsername()) !== 0 or strlen($this->apiClient->getConfig()->getPassword()) !== 0) {
+            $headerParams['Authorization'] = 'Basic ' . base64_encode($this->apiClient->getConfig()->getUsername() . ":" . $this->apiClient->getConfig()->getPassword());
+        }
+        // make the API Call
+        try {
+            list($response, $statusCode, $httpHeader) = $this->apiClient->callApi(
+                $resourcePath,
+                'DELETE',
+                $queryParams,
+                $httpBody,
+                $headerParams,
+                '\Reepay\Model\Session',
+                '/v1/session/{id}'
+            );
+
+            return [$this->apiClient->getSerializer()->deserialize($response, '\Reepay\Model\Session', $httpHeader), $statusCode, $httpHeader];
+        } catch (ApiException $e) {
+            switch ($e->getCode()) {
+                case 200:
+                    $data = $this->apiClient->getSerializer()->deserialize($e->getResponseBody(), '\Reepay\Model\Session', $e->getResponseHeaders());
+                    $e->setResponseObject($data);
+                    break;
+                case 400:
+                    $data = $this->apiClient->getSerializer()->deserialize($e->getResponseBody(), '\Reepay\Model\ErrorResponse', $e->getResponseHeaders());
+                    $e->setResponseObject($data);
+                    break;
+                case 401:
+                    $data = $this->apiClient->getSerializer()->deserialize($e->getResponseBody(), '\Reepay\Model\ErrorResponse', $e->getResponseHeaders());
+                    $e->setResponseObject($data);
+                    break;
+                case 403:
+                    $data = $this->apiClient->getSerializer()->deserialize($e->getResponseBody(), '\Reepay\Model\ErrorResponse', $e->getResponseHeaders());
+                    $e->setResponseObject($data);
+                    break;
+                case 404:
+                    $data = $this->apiClient->getSerializer()->deserialize($e->getResponseBody(), '\Reepay\Model\ErrorResponse', $e->getResponseHeaders());
+                    $e->setResponseObject($data);
+                    break;
+                case 500:
+                    $data = $this->apiClient->getSerializer()->deserialize($e->getResponseBody(), '\Reepay\Model\ErrorResponse', $e->getResponseHeaders());
+                    $e->setResponseObject($data);
+                    break;
+            }
+
+            throw $e;
+        }
+    }
 }
